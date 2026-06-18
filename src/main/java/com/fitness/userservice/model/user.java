ec8a06ad61310941b134c90ffd67fe3ebf15ddab
Rohan Kumar Sahu <sahurohankumar47@gmail.com>
2026-06-18 15:10:11 +0530

    DB and project setup done

---
+++ src/main/java/com/fitness/userservice/model/user.java
@@ -0,0 +1,36 @@
+package com.fitness.userservice.model;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="users")
+public class user {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(nullable = false,unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String firstName;
+    private String lastName;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role = UserRole.USER;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+
+}
